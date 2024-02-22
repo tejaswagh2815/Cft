02cf3becf6831ceec4e66d638c5735cafe9bdf85
tejas wagh <tejaswagh@tejass-MacBook-Air.local>
2024-02-22 12:45:47 +0530

    Login and Signup setting with formik and yup

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,13 +1,76 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Button } from 'react-native-paper'
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import {Button, TextInput} from 'react-native-paper';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
 
-const Login = () => {
+const validation = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required(),
+  password: Yup.string().required().min(4, 'password is short'),
+});
+
+const Login = ({navigation}) => {
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontSize: 20}}>Login</Text>
+      <Formik
+        initialValues={{email: '', password: ''}}
+        validationSchema={validation}
+        onSubmit={values => console.log(values)}>
+        {({handleChange, handleBlur, handleSubmit, values, errors}) => (
+          <View style={{width: '90%'}}>
+            <Text style={styles.title}>Email Id</Text>
+            <TextInput
+              style={styles.txtinput}
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              value={values.email}
+            />
+            {errors.email && (
+              <Text style={styles.errortxt}>{errors.email}</Text>
+            )}
+            <Text style={[styles.title, {paddingTop: 10}]}>Password</Text>
+            <TextInput
+              style={styles.txtinput}
+              onChangeText={handleChange('password')}
+              onBlur={handleBlur('password')}
+              value={values.password}
+            />
+            {errors.password && (
+              <Text style={styles.errortxt}>{errors.password}</Text>
+            )}
+            <Button
+              style={{marginTop: 20}}
+              mode="contained"
+              onPress={handleSubmit}
+              title="Submit">
+              Submit
+            </Button>
+            <Button
+              style={{marginTop: 20}}
+              onPress={() => navigation.navigate('Signup')}>
+              goto Sign Up
+            </Button>
+          </View>
+        )}
+      </Formik>
     </View>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    fontWeight: '300',
+    color: '#000',
+  },
+  txtinput: {
+    width: '100%',
+    alignSelf: 'center',
+  },
+  errortxt: {
+    color: 'red',
+  },
+});
--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -1,12 +1,84 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import {Button, TextInput} from 'react-native-paper';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
 
-const Signup = () => {
+const validation = Yup.object().shape({
+  name: Yup.string().required(),
+  email: Yup.string().email('Invalid email').required(),
+  password: Yup.string().required().min(4, 'password is short'),
+});
+const Signup = ({navigation}) => {
   return (
-    <View>
-      <Text>Signup</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontSize: 20}}>Register</Text>
+      <Formik
+        initialValues={{name: '', email: '', password: ''}}
+        validationSchema={validation}
+        onSubmit={values => console.log(values)}>
+        {({handleChange, handleBlur, handleSubmit, values, errors}) => (
+          <View style={{width: '90%'}}>
+            <Text style={styles.title}>Name</Text>
+            <TextInput
+              style={styles.txtinput}
+              onChangeText={handleChange('name')}
+              onBlur={handleBlur('name')}
+              value={values.name}
+            />
+            {errors.name && <Text style={styles.errortxt}>{errors.name}</Text>}
+            <Text style={[styles.title, {paddingTop: 10}]}>Email Id</Text>
+            <TextInput
+              style={styles.txtinput}
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              value={values.email}
+            />
+            {errors.email && (
+              <Text style={styles.errortxt}>{errors.email}</Text>
+            )}
+            <Text style={[styles.title, {paddingTop: 10}]}>Password</Text>
+            <TextInput
+              style={styles.txtinput}
+              onChangeText={handleChange('password')}
+              onBlur={handleBlur('password')}
+              value={values.password}
+            />
+            {errors.password && (
+              <Text style={styles.errortxt}>{errors.password}</Text>
+            )}
+            <Button
+              style={{marginTop: 20}}
+              mode="contained"
+              onPress={handleSubmit}
+              title="Submit">
+              Submit
+            </Button>
+            <Button
+              style={{marginTop: 20}}
+              onPress={() => navigation.navigate('Login')}>
+              Back to Login
+            </Button>
+          </View>
+        )}
+      </Formik>
     </View>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    fontWeight: '300',
+    color: '#000',
+  },
+  txtinput: {
+    width: '100%',
+    alignSelf: 'center',
+  },
+  errortxt: {
+    color: 'red',
+  },
+});
